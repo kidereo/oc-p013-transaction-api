5e83f44761ae4a53c6bfe2851cef3fe07e52e2dd
kidereo <kirill.derevenski@gmail.com>
2022-12-02 15:43:32 +0100

    Implemented GET accounts and GET transactions.

--- app/Http/Resources/V1/TransactionCollection.php
+++ app/Http/Resources/V1/TransactionCollection.php
@@ -4,16 +4,16 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class TransactionCollection extends ResourceCollection
-{
+class TransactionCollection extends ResourceCollection {
+
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return parent ::toArray($request);
     }
 }
